8a3161ad55df4ba00a7247d9882aeea713698f54
AhmedShawky93 <ahmedsh218@gmail.com>
2022-10-19 16:10:00 +0200

    saideBar

--- resources/views/layout/dashboard.blade.php
+++ resources/views/layout/dashboard.blade.php
@@ -1,8 +1,8 @@
-<div class="content-start transition">
-    <div class="container-fluid dashboard">
-        <div class="content-header">
-            <h1>Al Mansour Holding Co. For Financial Investments</h1>
-            <p></p>
+<div class="main">
+    <div class="main-content container-fluid">
+        <div class="header-top">
+            <p class="SideBarTitle info">@yield('title')</p>
         </div>
+        <h1>Al Mansour Holding Co. For Financial Investments</h1>
     </div>
 </div>
