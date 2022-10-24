e53221ff4223f18cfb86ce93d4b2161843c2cbca
MUHNANAD <muhanadmahmoud@hotmail.com>
2022-10-24 08:52:20 +0200

    git add .

--- app/helpers.php
+++ app/helpers.php
@@ -51,7 +51,7 @@ if (!function_exists('get_data_pivot_table')) {
     if (!function_exists('helper_update_table')) {
         function helper_update_table($SALESREP_ID, $TAB_NAME, $RUN_CODE)
         {
-            DB::insert('insert into SYNC_DATA_SALESREP_TEST@SALES (SALESREP_ID, TAB_NAME, RUN_CODE) values (?, ?,?)', [
+            DB::insert('insert into SYNC_DATA_SALESREP@SALES (SALESREP_ID, TAB_NAME, RUN_CODE) values (?, ?,?)', [
                 $SALESREP_ID, $TAB_NAME, $RUN_CODE
             ]);
         }
